c74e09f9b57fe7f86f4071d1dd7c689e7da1b26d
oszust <jakub.oszust@nokia.com>
2019-10-09 01:02:20 +0200

    Add project files.

---
+++ UP1/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace UP1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.playSound_button = new System.Windows.Forms.Button();
+            this.directSound_Button = new System.Windows.Forms.Button();
+            this.readbytes_button = new System.Windows.Forms.Button();
+            this.wavHeader_textBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // playSound_button
+            // 
+            this.playSound_button.Location = new System.Drawing.Point(675, 12);
+            this.playSound_button.Name = "playSound_button";
+            this.playSound_button.Size = new System.Drawing.Size(113, 59);
+            this.playSound_button.TabIndex = 0;
+            this.playSound_button.Text = "PlaySound func";
+            this.playSound_button.UseVisualStyleBackColor = true;
+            this.playSound_button.Click += new System.EventHandler(this.PlaySound_button_Click);
+            // 
+            // directSound_Button
+            // 
+            this.directSound_Button.Location = new System.Drawing.Point(537, 12);
+            this.directSound_Button.Name = "directSound_Button";
+            this.directSound_Button.Size = new System.Drawing.Size(101, 59);
+            this.directSound_Button.TabIndex = 1;
+            this.directSound_Button.Text = "DirectSound";
+            this.directSound_Button.UseVisualStyleBackColor = true;
+            this.directSound_Button.Click += new System.EventHandler(this.DirectSound_Button_Click);
+            // 
+            // readbytes_button
+            // 
+            this.readbytes_button.Location = new System.Drawing.Point(414, 12);
+            this.readbytes_button.Name = "readbytes_button";
+            this.readbytes_button.Size = new System.Drawing.Size(101, 59);
+            this.readbytes_button.TabIndex = 2;
+            this.readbytes_button.Text = "Read wav Bytes";
+            this.readbytes_button.UseVisualStyleBackColor = true;
+            this.readbytes_button.Click += new System.EventHandler(this.Readbytes_button_Click);
+            // 
+            // wavHeader_textBox
+            // 
+            this.wavHeader_textBox.Location = new System.Drawing.Point(42, 28);
+            this.wavHeader_textBox.Multiline = true;
+            this.wavHeader_textBox.Name = "wavHeader_textBox";
+            this.wavHeader_textBox.Size = new System.Drawing.Size(199, 74);
+            this.wavHeader_textBox.TabIndex = 3;
+            this.wavHeader_textBox.Text = "There should be wav header values.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.wavHeader_textBox);
+            this.Controls.Add(this.readbytes_button);
+            this.Controls.Add(this.directSound_Button);
+            this.Controls.Add(this.playSound_button);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playSound_button;
+        private System.Windows.Forms.Button directSound_Button;
+        private System.Windows.Forms.Button readbytes_button;
+        private System.Windows.Forms.TextBox wavHeader_textBox;
+    }
+}
+
---
+++ UP1/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using Microsoft.DirectX.DirectSound;
+using System.IO;
+namespace UP1
+{
+    public partial class Form1 : Form
+    {
+
+        [DllImport("winmm.DLL", EntryPoint = "PlaySound", SetLastError = true, CharSet = CharSet.Unicode, ThrowOnUnmappableChar = true)]
+        private static extern bool PlaySound(string szSound, IntPtr hMod, PlaySoundFlags flags);
+
+
+        [Flags]
+        public enum PlaySoundFlags : int
+        {
+            SND_SYNC = 0x0000,
+            SND_ASYNC = 0x0001,
+            SND_NODEFAULT = 0x0002,
+            SND_LOOP = 0x0008,
+            SND_NOSTOP = 0x0010,
+            SND_NOWAIT = 0x00002000,
+            SND_FILENAME = 0x00020000,
+            SND_RESOURCE = 0x00040004
+        }
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void PlaySound_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog1 = new OpenFileDialog();
+
+            dialog1.Title = "Browse to find sound file to play";
+            dialog1.InitialDirectory = @"c:\";
+            dialog1.Filter = "Wav Files (*.wav)|*.wav";
+            dialog1.FilterIndex = 2;
+            dialog1.RestoreDirectory = true;
+
+            if (dialog1.ShowDialog() == DialogResult.OK)
+            {
+                PlaySound(dialog1.FileName, new System.IntPtr(), PlaySoundFlags.SND_SYNC);
+            }
+        }
+
+        private void DirectSound_Button_Click(object sender, EventArgs e)
+        {
+            var device = new Device();
+            device.SetCooperativeLevel(this, CooperativeLevel.Priority);
+            SecondaryBuffer wavSound = new SecondaryBuffer(@"C:\Users\oszust\source\repos\UP1\UP1\example.wav", device);
+            wavSound.Play(0, BufferPlayFlags.Default);
+        }
+
+        private void Readbytes_button_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog1 = new OpenFileDialog();
+
+            dialog1.Title = "Browse to find sound file to play";
+            dialog1.InitialDirectory = @"c:\";
+            dialog1.Filter = "Wav Files (*.wav)|*.wav";
+            dialog1.FilterIndex = 2;
+            dialog1.RestoreDirectory = true;
+
+            if (dialog1.ShowDialog() == DialogResult.OK)
+            {
+                byte[] wavFile = File.ReadAllBytes(dialog1.FileName);
+                byte[] wavHeader = new byte[44];
+                
+                for (int i = 0; i < 44; i++)
+                {
+                    wavHeader[i] = wavFile[i];
+                }
+                // Czytać bajty po jednym, nie tak jak poniżej
+
+                byte[] riffDescriptionByteArray = new byte[4] { wavHeader[0], wavHeader[1], wavHeader[2], wavHeader[3] };
+                byte[] sizeOfFileByteArray = new byte[4] { wavHeader[4], wavHeader[5], wavHeader[6], wavHeader[7] };
+                byte[] wavDescriptionByteArray = new byte[4] { wavHeader[8], wavHeader[9], wavHeader[10], wavHeader[11] };
+                byte[] fmtDescriptionByteArray = new byte[4] { wavHeader[12], wavHeader[13], wavHeader[14], wavHeader[15] };
+
+                //string riffDescription = Encoding.ASCII.GetString(riffDescriptionByteArray);
+                //string sizeOfFile = Encoding.ASCII.GetString(sizeOfFileByteArray);
+                //string wavDescription = Encoding.ASCII.GetString(wavDescriptionByteArray);
+                //string fmtDescription = Encoding.ASCII.GetString(fmtDescriptionByteArray);
+
+                //wavHeader_textBox.Text = riffDescription + " " + sizeOfFile + " " + wavDescription + " " + fmtDescription;
+            }
+        }
+    }
+}
